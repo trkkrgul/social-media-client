52a1c387d63980cc8b197c7d27f06208e6b69594
trkkrgul <109871858+trkkrgul@users.noreply.github.com>
2023-04-23 16:57:06 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["storage.googleapis.com"],
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+      },
+    ],
   },
 };
 
