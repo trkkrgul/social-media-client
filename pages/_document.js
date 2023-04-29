59bee1f7f3e6b913dd597430b47a9c3df74ac0e4
trkkrgul <122627006+SakaiVault@users.noreply.github.com>
2023-04-29 17:46:22 +0300

    ksadk

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
           content="DeFiTalks | World's first DeFi social media platform."
