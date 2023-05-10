f4481e8461c446eec63e2d0f70a753aa0de3bebf
Barış Ertakuş <bariserta@gmail.com>
2023-05-10 19:23:45 +0200

    Add modal to ImageGallery

--- components/images/ImageGallery/index.jsx
+++ components/images/ImageGallery/index.jsx
@@ -3,12 +3,21 @@ import { FaDownload } from "react-icons/fa";
 import { Card, CardBody, CardFooter, CardHeader } from "@saas-ui/react";
 import {
   Box,
+  Button,
   Flex,
   HStack,
   Icon,
   IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spacer,
   useColorMode,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 import { IoCloseCircle, IoCloseCircleOutline } from "react-icons/io5";
@@ -19,14 +28,21 @@ const ImageGallery = ({ images }) => {
   const [previewIndex, setPreviewIndex] = useState(0);
   const [dragStartX, setDragStartX] = useState(null);
   const { colorMode } = useColorMode();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const openPreview = (index) => {
     setPreviewIndex(index);
-    setIsPreviewOpen(true);
+    // setIsPreviewOpen(true);
+
+    onOpen();
+
   };
 
   const closePreview = (event) => {
     event.stopPropagation(); // Prevent click event from propagating to the parent element
-    setIsPreviewOpen(false);
+    // setIsPreviewOpen(false);
+    onClose();
   };
 
   const downloadImage = (event) => {
@@ -90,113 +106,121 @@ const ImageGallery = ({ images }) => {
             />
           ))}{" "}
         </Flex>
-        {isPreviewOpen && (
-          <Box
-            backdropFilter={"auto"}
-            backdropBlur={"sm"}
-            onClick={closePreview}
-            position={"fixed"}
-            zIndex={"overlay"}
-            top={"0"}
-            left={"0"}
-            width={"100vw"}
-            height={"100vh"}
-            backgroundColor={" rgba(0, 0, 0, 0.7)"}
-            display={"flex"}
-            alignItems={"center"}
-            justifyContent={"center"}
-          >
-            <Card
-              onClick={(event) => event.stopPropagation()}
-              bg={colorMode === "dark" ? "gray.800" : "whiteAlpha.800"}
-              border={"1px solid"}
-              borderColor={"whiteAlpha.300"}
-              w={['90%', '90%', '900px']}
-            >
-              <CardHeader>
-                <HStack w={"100%"}>
-                  <Spacer />
-                  <IconButton
-                    variant={"ghost"}
-                    size={"lg"}
-                    icon={<IoCloseCircleOutline size={"24"} />}
-                    onClick={closePreview}
-                  />
-                </HStack>
-              </CardHeader>
-              <CardBody>
-                <HStack justifyContent={"space-between"}>
-                  <Icon
-                    fontSize={"24"}
-                    as={BsArrowLeftCircle}
-                    cursor={previewIndex === 0 ? "not-allowed" : "pointer"}
-                    onClick={() => {
-                      if (previewIndex === 0) {
-                        return;
-                      }
-                      setPreviewIndex((prev) => prev - 1);
-                    }}
-                  />
-
-                  {!!(images.length > 0) && (
-                    <Image
-                      src={images[previewIndex]}
-                      width={640} // ['79%', '79%', '640px']
-                      height={450}
-                      alt={`Image ${previewIndex + 1}`}
-                      style={{
-                        objectFit: 'contain',
-                        borderRadius: 10,
-                        maxHeight: '450px',
-                        maxWidth: '80%',
+        
+   
+  
+          <Modal isOpen={isOpen} onClose={onClose} size="full">
+            <ModalOverlay />
+            <ModalContent>
+             <Box
+         backdropFilter={"auto"}
+         backdropBlur={"sm"}
+         onClick={closePreview}
+         position={"fixed"}
+         zIndex={"overlay"}
+         top={"0"}
+         left={"0"}
+         width={"100vw"}
+         height={"100vh"}
+         backgroundColor={" rgba(0, 0, 0, 0.7)"}
+         display={"flex"}
+         alignItems={"center"}
+         justifyContent={"center"}
+       >
+              <Card
+                onClick={(event) => event.stopPropagation()}
+                bg={colorMode === "dark" ? "gray.800" : "whiteAlpha.800"}
+                border={"1px solid"}
+                borderColor={"whiteAlpha.300"}
+                w={['90%', '90%', '900px']}
+                className="essek"
+              >
+                <CardHeader>
+                  <HStack w={"100%"}>
+                    <Spacer />
+                    <IconButton
+                      variant={"ghost"}
+                      size={"lg"}
+                      icon={<IoCloseCircleOutline size={"24"} />}
+                      onClick={closePreview}
+                    />
+                  </HStack>
+                </CardHeader>
+                <CardBody>
+                  <HStack justifyContent={"space-between"}>
+                    <Icon
+                      fontSize={"24"}
+                      as={BsArrowLeftCircle}
+                      cursor={previewIndex === 0 ? "not-allowed" : "pointer"}
+                      onClick={() => {
+                        if (previewIndex === 0) {
+                          return;
+                        }
+                        setPreviewIndex((prev) => prev - 1);
                       }}
                     />
-                  )}
-                  <Icon
-                    fontSize={"24"}
-                    as={BsArrowRightCircle}
-                    cursor={
-                      previewIndex === images.length - 1
-                        ? "not-allowed"
-                        : "pointer"
-                    }
-                    onClick={() => {
-                      if (previewIndex === images.length - 1) {
-                        return;
-                      }
-                      setPreviewIndex((prev) => prev + 1);
-                    }}
-                  />
-                </HStack>
-              </CardBody>
-              <CardFooter>
-                <Flex
-                  justifyContent={"center"}
-                  width={"100%"}
-                  gap={"2"}
-                  alignItems={"center"}
-                >
-                  {images.map((src, index) => (
-                    <Box key={index} onClick={() => setPreviewIndex(index)}>
+
+                    {!!(images.length > 0) && (
                       <Image
-                        width={index === previewIndex ? 100 : 50}
-                        height={index === previewIndex ? 50 : 50}
+                        src={images[previewIndex]}
+                        width={640} // ['79%', '79%', '640px']
+                        height={450}
+                        alt={`Image ${previewIndex + 1}`}
                         style={{
-                          borderRadius: "10px",
-                          objectFit: "cover",
-                          width: index === previewIndex ? 100 : 50,
-                          height: index === previewIndex ? 50 : 50,
+                          objectFit: 'contain',
+                          borderRadius: 10,
+                          maxHeight: '450px',
+                          maxWidth: '80%',
                         }}
-                        src={src}
-                        alt={`Image ${index + 1}`}
                       />
-                    </Box>
-                  ))}
-                </Flex>
-              </CardFooter>
-            </Card>
-          </Box>
-        )}
+                    )}
+                    <Icon
+                      fontSize={"24"}
+                      as={BsArrowRightCircle}
+                      cursor={
+                        previewIndex === images.length - 1
+                          ? "not-allowed"
+                          : "pointer"
+                      }
+                      onClick={() => {
+                        if (previewIndex === images.length - 1) {
+                          return;
+                        }
+                        setPreviewIndex((prev) => prev + 1);
+                      }}
+                    />
+                  </HStack>
+                </CardBody>
+                <CardFooter>
+                  <Flex
+                    justifyContent={"center"}
+                    width={"100%"}
+                    gap={"2"}
+                    alignItems={"center"}
+                  >
+                    {images.map((src, index) => (
+                      <Box key={index} onClick={() => setPreviewIndex(index)}>
+                        <Image
+                          width={index === previewIndex ? 100 : 50}
+                          height={index === previewIndex ? 50 : 50}
+                          style={{
+                            borderRadius: "10px",
+                            objectFit: "cover",
+                            width: index === previewIndex ? 100 : 50,
+                            height: index === previewIndex ? 50 : 50,
+                          }}
+                          src={src}
+                          alt={`Image ${index + 1}`}
+                        />
+                      </Box>
+                    ))}
+                  </Flex>
+                </CardFooter>
+              </Card>
+              </Box>
+            </ModalContent>
+          </Modal>
+        
       </div>
     </Suspense>
   );
